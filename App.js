c8b123bb4e9601957b70e65b84e1d2397660f7ef
Sakshya Arora <saksh@Sakshyas-MacBook-Pro.local>
2022-11-22 14:54:56 +0530

    feat: dynamic icon working on android

--- App.js
+++ App.js
@@ -1,116 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Image,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  TouchableWithoutFeedback,
 } from 'react-native';
+import {changeIcon, getIcon} from 'react-native-change-icon';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [currentIcon, setCurrentIcon] = useState('logred');
+  const changeAppIcon = async name => {
+    try {
+      const response = await changeIcon(name);
+      setCurrentIcon(response);
+    } catch (err) {}
+  };
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    const getCurrentIcon = async () => {
+      const currentIcon = await getIcon();
+      setCurrentIcon(currentIcon);
+    };
+    getCurrentIcon();
+  }, []);
+
+  const iconsList = [
+    {id: 1, iconName: 'logored', image: require(`./src/assets/logored.png`)},
+    {
+      id: 2,
+      iconName: 'logocyan',
+      image: require(`./src/assets/logocyan.png`),
+    },
+    {
+      id: 3,
+      iconName: 'logopurple',
+      image: require(`./src/assets/logopurple.png`),
+    },
+    {
+      id: 4,
+      iconName: 'logoorange',
+      image: require(`./src/assets/logoorange.png`),
+    },
+    {id: 5, iconName: 'logopink', image: require(`./src/assets/logopink.png`)},
+    {id: 6, iconName: 'logogray', image: require(`./src/assets/logogray.png`)},
+    {
+      id: 7,
+      iconName: 'logogreen',
+      image: require(`./src/assets/logogreen.png`),
+    },
+  ];
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1}}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>Select your App Icon</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        {iconsList.map(icon => {
+          const isSelected = icon.iconName === currentIcon;
+          console.log('isSelected>>>>>', isSelected);
+          return (
+            <TouchableWithoutFeedback
+              key={icon.id}
+              onPress={() => changeAppIcon(icon.iconName)}>
+              <View
+                style={{
+                  backgroundColor: isSelected ? 'lightgray' : 'transparent',
+                  borderRadius: 10,
+                  margin: 4,
+                }}>
+                <Image style={styles.icon} source={icon.image} />
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </View>
+      <Text>Current Icon : {currentIcon}</Text>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  headingContainer: {padding: 8, marginTop: 20},
+  heading: {
+    fontWeight: '500',
+    fontSize: 20,
+    color: '#000',
+    textAlign: 'center',
+    marginBottom: 16,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  icon: {
+    height: 100,
+    borderRadius: 10,
+    width: 100,
+    margin: 8,
   },
 });
 
